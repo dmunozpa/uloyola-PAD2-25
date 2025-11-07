379f9752fe2c2acf31429db6e4fd4c8740da8595
Daniel Muñoz <dmunoz@uloyola.es>
2025-11-07 14:47:57 +0100

    Tema 3 - Ejercicios JSON

--- T3/Ejercicio7.py
+++ T3/Ejercicio7.py
@@ -1,6 +1,7 @@
 import json
 
 ruta_fichero_usuarios = "T3/data/usuarios.json"
+ruta_fichero_usuarios_modificado = "T3/data/usuarios_modificado.json"
 
 # 1. Leer el archivo JSON completo
 with open(ruta_fichero_usuarios, "r", encoding="utf-8") as f:
@@ -11,18 +12,18 @@ for usuario in usuarios:
     # Incrementar edad si está activo
     if usuario["activo"]:
         usuario["edad"] += 1
-
-    # Añadir categoría según la edad
-    edad = usuario["edad"]
-    if edad < 30:
-        usuario["categoria"] = "joven"
-    elif edad < 60:
-        usuario["categoria"] = "adulto"
-    else:
-        usuario["categoria"] = "senior"
+        
+        # Añadir categoría según la edad
+        edad = usuario["edad"]
+        if edad < 30:
+            usuario["categoria"] = "joven"
+        elif edad < 60:
+            usuario["categoria"] = "adulto"
+        else:
+            usuario["categoria"] = "senior"
 
 # 3. Guardar los cambios en un nuevo archivo
-with open(ruta_fichero_usuarios, "w", encoding="utf-8") as f:
+with open(ruta_fichero_usuarios_modificado, "w", encoding="utf-8") as f:
     json.dump(usuarios, f, indent=4)
 
 print(f"Archivo '{ruta_fichero_usuarios}' generado correctamente.")
